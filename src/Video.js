5b012482fabd8d693fcca971fdc850a09c7f1b11
thomassjaastad <thomas.sjaastad@gmail.com>
2021-01-29 17:33:46 +0100

    add video to archive

---
+++ src/Video.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Bjorg.css";
+
+const Video = () => {
+  return (
+    <div>
+      <p className="urlcenter"> Herotic - Welcome to paradise </p>
+    </div>
+  );
+};
+
+export default Video;
